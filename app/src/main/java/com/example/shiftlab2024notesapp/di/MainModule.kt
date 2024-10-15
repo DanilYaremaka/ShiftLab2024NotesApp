cb38a8787ec795a5a45e64f2d15553bc1283d726
DanilUltrakill <yaremakadanil@gmail.com>
2024-10-16 02:33:51 +0700

    добавление навигации для экрана заметок

--- app/src/main/java/com/example/shiftlab2024notesapp/di/MainModule.kt
+++
@@ -1,7 +0,0 @@
-package com.example.shiftlab2024notesapp.di
-
-import org.koin.dsl.module
-
-val mainModule = module {
-
-}
--- app/src/main/java/com/example/shiftlab2024notesapp/ui/MainScreen.kt
+++ app/src/main/java/com/example/shiftlab2024notesapp/ui/MainScreen.kt
@@ -1,8 +1,51 @@
 package com.example.shiftlab2024notesapp.ui
 
+import NotesRoute
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.shiftlab2024notesapp.navigation.NavControllerHolder
+import com.example.shiftlab2024notesapp.notes.ui.NotesScreen
+import org.koin.compose.koinInject
 
 @Composable
 fun MainScreen() {
 
+    val navController = rememberNavController()
+    val navControllerHolder = koinInject<NavControllerHolder>()
+
+    DisposableEffect(key1 = Unit) {
+        navControllerHolder.setNavController(navController)
+        onDispose {
+            navControllerHolder.clearNavController()
+        }
+    }
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = { }) {
+                Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End
+    ) { paddingValues ->
+        NavHost(
+            navController = navController,
+            startDestination = NotesRoute
+        ) {
+            composable<NotesRoute> {
+                NotesScreen(
+                    paddingValues = paddingValues
+                )
+            }
+        }
+    }
 }
