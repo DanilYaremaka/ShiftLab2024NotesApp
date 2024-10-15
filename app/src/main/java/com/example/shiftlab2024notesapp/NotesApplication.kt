aa1161f666a303f91f6d4495490c0bfa205ee968
DanilUltrakill <yaremakadanil@gmail.com>
2024-10-15 20:46:41 +0700

    di Koin

--- app/src/main/java/com/example/shiftlab2024notesapp/NotesApplication.kt
+++ app/src/main/java/com/example/shiftlab2024notesapp/NotesApplication.kt
@@ -1,11 +1,25 @@
 package com.example.shiftlab2024notesapp
 
 import android.app.Application
+import com.example.shiftlab2024notesapp.di.mainModule
+import com.example.shiftlab2024notesapp.notes.di.notesModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class NotesApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
 
+        startKoin {
+            androidLogger(level = Level.DEBUG)
+            androidContext(this@NotesApplication)
+            modules(
+                mainModule,
+                notesModule
+            )
+        }
     }
 }
---
+++ app/src/main/java/com/example/shiftlab2024notesapp/di/MainModule.kt
@@ -0,0 +1,7 @@
+package com.example.shiftlab2024notesapp.di
+
+import org.koin.dsl.module
+
+val mainModule = module {
+
+}
