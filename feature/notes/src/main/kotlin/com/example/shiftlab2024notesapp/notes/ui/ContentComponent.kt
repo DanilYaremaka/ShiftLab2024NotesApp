27f4de331d5e08fce80a358aee85962460315fc7
DanilUltrakill <yaremakadanil@gmail.com>
2024-10-20 01:14:20 +0700

    заменил ... на соответсвующий символ и убрал ненужную строку

--- feature/notes/src/main/kotlin/com/example/shiftlab2024notesapp/notes/ui/ContentComponent.kt
+++ feature/notes/src/main/kotlin/com/example/shiftlab2024notesapp/notes/ui/ContentComponent.kt
@@ -37,7 +37,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -63,7 +62,6 @@ fun ContentComponent(
     searchQuery: String,
     searchQueryChanged: (String) -> Unit
 ) {
-    val scope = rememberCoroutineScope()
 
     Scaffold(
         topBar = {
